a1f87a6a2d06e93d34306231992acb5b3e1c2de8
efoneli <abbaefoneli@gmail.com>
2023-08-27 15:16:01 +0100

    header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import { useEffect, useRef } from "react";
+import app from "../../assets/images/portfolio-07.png";
+import { NavLink, Link } from "react-router-dom";
+
+const navlinks = [
+  {
+    path: "./home",
+    display: "Home",
+  },
+  {
+    path: "./doctors",
+    display: "Find a Doctor",
+  },
+  {
+    path: "./services",
+    display: "Services",
+  },
+  {
+    path: "./contact",
+    display: "Contact",
+  },
+];
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="header flex items-center">
+      <div className="container">
+        <div className="flex tems-center justify-between">
+          <div>
+            <img src={app} alt="" />
+          </div>
+          <div className="navigation">
+            <ul className="menu flex items-center gap-[2.7rem">
+              {navlinks.map((link, index) => (
+                <li key={index}>
+                  <NavLink
+                    to={link.path}
+                    className={(navClass) =>
+                      navClass.isActive
+                        ? "text-primaryColor text-[16px] leading-7 font-[600]"
+                        : "text-textColor text-[16px] leading-7 font-[500]"
+                    }
+                  >
+                    {link.display}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
