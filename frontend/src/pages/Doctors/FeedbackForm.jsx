99a5f0cedba250e291296edfbc636e7f0182aeba
Efoneli <abbaefoneli@gmail.com>
2023-09-15 20:55:03 +0100

    login and signup

--- frontend/src/pages/Doctors/FeedbackForm.jsx
+++ frontend/src/pages/Doctors/FeedbackForm.jsx
@@ -1,20 +1,26 @@
 import { useState } from "react";
 import { AiFillStar } from "react-icons/ai";
 
-const FeedbackForm = () => {
 
+const FeedbackForm = () => {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
+  const [reviewText, setReviewText] = useState("");
+  
+  const handleSubmitReview = async e => {
+    e.prventDefault();
+  }
 
   return (
     <form action="">
       <div>
-        <h3 className="text-headingColor text-[16px] leading-6 font-semibold mb-4">
-          How would you rate the overall experience 👍
+        <h3 className="text-headingColor text-[16px] leading-6 font-semibold mb-4 mt-0">
+          Share your feedback or suggestions*
         </h3>
 
+        
         <div>
-          {[...Array(5).keys()].map((_,index) => {
+          {[...Array(5).keys()].map((_, index) => {
             index += 1;
 
             return (
@@ -25,8 +31,8 @@ const FeedbackForm = () => {
                   index <= ((rating && hover) || hover)
                     ? "text-yellowColor"
                     : "text-gray-400"
-                } bg-transparent border-none outline-one text-[22px] cursor-pointer `}
-                onClick={setRating(index)}
+                } bg-transparent border-none outline-none text-[22px] cursor-pointer`}
+                onClick={() => setRating(index)}
                 onMouseEnter={() => setHover(index)}
                 onMouseLeave={() => setHover(rating)}
                 onDoubleClick={() => {
@@ -39,9 +45,22 @@ const FeedbackForm = () => {
                 </span>
               </button>
             );
-          })}
+          })} 
         </div>
       </div>
+
+      <div className="mt-[30px]">
+        <h3 className="text-headingColor text-[16px] leading-6 font-semibold mb-4 mt-0">
+          How would you rate the overall experience?*
+        </h3>
+
+        <textarea className="border border-solid border-[#0066ff34] focus:outline outline outline-primaryColor w-full px-4" rows="5" placeholder="Write your message" onChange={()=>setReviewText(e.target.value)}>
+
+        </textarea>
+      </div>
+      <button type="submit" className="btn" onClick={handleSubmitReview}>
+        Submit Feedback
+      </button>
     </form>
   );
 };
