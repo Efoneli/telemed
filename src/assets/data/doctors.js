8540f85321810c9285f7fd4e3c3e4705996ffc68
Efoneli <abbaefoneli@gmail.com>
2023-09-21 15:59:14 +0100

    today

--- src/assets/data/doctors.js
+++ src/assets/data/doctors.js
@@ -1,6 +1,7 @@
 import doctorImg01 from "../images/doctor-img01.png";
 import doctorImg02 from "../images/doctor-img02.png";
 import doctorImg03 from "../images/doctor-img03.png";
+import avgwoman from '../images/avgwoman.jpg'
 
 export const doctors = [
   {
@@ -14,14 +15,34 @@ export const doctors = [
     hospital: "Mount Adora Hospital, Wuse 2.",
   },
   {
+    id: "03",
+    name: "Dr. Farid Uddin",
+    specialization: "Dermatologist",
+    avgRating: 4.4,
+    totalRating: 279,
+    photo: doctorImg03,
+    totalPatients: 800,
+    hospital: "Padre Pio Hospital, Karu.",
+  },
+  {
     id: "02",
     name: "Dr. Saleh Mahmud",
     specialization: "Neurologist",
     avgRating: 4.6,
     totalRating: 362,
-    photo: doctorImg02,
+    photo: avgwoman,
     totalPatients: 1200,
     hospital: "General Hospital, Maitama.",
+  },
+  {
+    id: "03",
+    name: "Dr. Farid Uddin",
+    specialization: "Dermatologist",
+    avgRating: 4.4,
+    totalRating: 279,
+    photo: doctorImg03,
+    totalPatients: 800,
+    hospital: "Padre Pio Hospital, Karu.",
   },
   {
     id: "03",
